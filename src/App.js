43c710e124ce31bccf78ce5c691f72aa72ff561c
Gulnaz <viteewato@mail.ru>
2024-08-05 05:42:42 +0300

    Calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
- import logo from './logo.svg';
-//import React from 'react';
-import { createElement } from 'react';
-import './App.css';
 
-function getYear() {
-  let date = new Date();
-  let year = date.getFullYear()
-  return createElement('p',null, {year})
-}
-
+import styles from  './App.module.css';
+import { useState } from 'react';
 
 export const App = () => {
-		return createElement('div',createElement('header', {className: 'App-header'},
-												createElement('img', {className : 'App-logo',src : {logo}}, null),
-												createElement ('p',null,'Edit',
-												createElement('code',null, 'src/App.js' ), 'and save to reload.'),
-												createElement('a',{className: 'App-link',href:'https://reactjs.org', target:'_blank', rel: 'noopener noreferrer'  }, 'Learn React'),
-												getYear,
-												null)
-												,{className : 'App'}, null)
-}
+
+
+	const [result, setResult] = useState(0);
+	const [state, setState]  = useState( '' );
+
+	const buttons = [
+		{id:0, name: '0'},
+		{id:1, name: '1'},
+		 {id:2, name: '2'},
+		  {id:3, name: '3'},
+		  {id:4, name: '4'},
+		  {id:5, name: '5'},
+		  {id:6, name: '6'},
+		  {id:7, name: '7'},
+		  {id:8, name: '8'},
+		  {id:9, name: '9'},
+		  {id:10, name: '+'},
+		  {id:11, name: '-'},
+		  {id:12, name: '*'},
+		  {id:13, name: '/'},
+		  {id:14, name: '='},
+		  {id:15, name: 'c'}
+	];
+
+	const calc = (expression) => {
+  		var func = new Function("return " + expression);
+  		return func();
+	}
+
+	const updateResult = (action) => {
+		 let q = calc(action);
+		setState((result + q))
+	}
+
+
+	const inputOnChange = (num) => {
+		let value = '';
+		buttons.filter(({id,name}) => (id === num ) ? value = name : undefined)
+
+			if (!isNaN(Number(value)))  {
+				setState(state => state +''+Number(value))
+			}
+			if ( value === '/' || value === '*' || value === '+' || value === '-' ){
+
+				setState(state => state +''+value)
+
+			}
+			if (value === '=' ){
+				updateResult(state)
+			}
+			if (value === 'c' ) {
+				setState('');
+			}
+		}
+
+	return (
+		<div className = {styles.interface}>
+		 <label>RESULT</label>
+		 <input value={state}
+		 		className={styles.interfaceInput}
+		 		readOnly
+			/>
+			<div className={styles.buttonContainer}>
+		 		{buttons.map( ({ id, name }) => <button
+				onClick={() => {inputOnChange(id)}}
+				key={id}
+				className={styles.button}>
+				{name}
+		 		</button>
+		 			)}
+			</div>
+		 <span className={styles.span}>The calculator</span>
+		</div>
+	)
+	}
 
 
 
